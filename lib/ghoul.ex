d47e26e7c3afcbcdae8580f9c544b1f1eac606ef
Chris Meyer <meyer.cm@gmail.com>
2017-08-13 08:28:58 -0400

    initial commit

---
+++ lib/ghoul.ex
@@ -0,0 +1,18 @@
+defmodule Ghoul do
+  @moduledoc """
+  Documentation for Ghoul.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ghoul.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ghoul_test.exs
@@ -0,0 +1,8 @@
+defmodule GhoulTest do
+  use ExUnit.Case
+  doctest Ghoul
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
